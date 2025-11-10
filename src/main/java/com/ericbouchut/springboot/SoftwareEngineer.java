6a370d4bced6e3eb237cc748f8552c84606debdf
Eric Bouchut <ebouchut@gmail.com>
2025-11-10 15:10:14 +0100

    feat: Add a home-made SoftwareEngineer "entity" class

---
+++ src/main/java/com/ericbouchut/springboot/SoftwareEngineer.java
@@ -0,0 +1,66 @@
+package com.ericbouchut.springboot;
+
+
+import java.util.Objects;
+
+/**
+ * Handmade "Entity" class (not persisted nor a Spring Boot Entity).
+ *
+ */
+public class SoftwareEngineer {
+    private Integer id;
+    private String name;
+    private String techStack;
+
+    public SoftwareEngineer() {
+    }
+
+    public SoftwareEngineer(Integer id, String name, String techStack) {
+        this();
+
+        this.techStack = techStack;
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTechStack() {
+        return techStack;
+    }
+
+    public void setTechStack(String techStack) {
+        this.techStack = techStack;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        SoftwareEngineer that = (SoftwareEngineer) o;
+        return Objects.equals(id, that.id)
+                && Objects.equals(name, that.name)
+                && Objects.equals(techStack, techStack);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, techStack);
+    }
+}
